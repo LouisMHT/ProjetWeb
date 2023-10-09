268fea29573700732b48fcc15665825b1e7c7eab
THIERRY Louis <louis.thierry@groupe-esigelec.org>
2023-10-09 12:06:42 +0200

    Ajout logo et code dans Louis

---
+++ Louis/pageAccueil.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+    <style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+}
+
+.header {
+  background-color: #111;
+  padding: 30px;
+  text-align: center;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+body {
+  background-image: url("jeuplateau.jpeg");
+  background-repeat: no-repeat;
+}
+
+</style>
+</head>
+<body>
+
+<div class="header">
+  <img src="Game-Ultimate-09-10-2023.png" alt="Logo" width=15%;>
+  
+</div>
+
+<ul>
+  <li><a  href="#home">Accueil</a></li>
+  <li><a href="#news">Jeux</a></li>
+  <li><a href="#planning">Planning</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a  href="#about">Connexion/Inscription</a></li>
+</ul>
+<!--<img src="jeuplateau.jpeg" alt="Jeu plateau">-->
+</body>
+</html>
