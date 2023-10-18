8dd040f22c39561b3232c0f2faf56433299669cd
LouisMHT <128934086+LouisMHT@users.noreply.github.com>
2023-10-18 14:04:36 +0200

    Ajout page web

--- Louis/pageAccueil.php
+++ Louis/pageAccueil.php
@@ -1,52 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Document</title>
-    <style>
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-  position: -webkit-sticky; /* Safari */
-  position: sticky;
-  top: 0;
-}
+<?php
+$titre = "Titre de la page";
+require('header.inc.php')
+?>
 
-.header {
-  background-color: #111;
-  padding: 30px;
-  text-align: center;
-}
 
-li {
-  float: left;
-}
-
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-li a:hover {
-  background-color: #111;
-}
-body {
-  background-image: url("jeuplateau.jpeg");
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-</style>
-</head>
 <body>
 
 <div class="header">
