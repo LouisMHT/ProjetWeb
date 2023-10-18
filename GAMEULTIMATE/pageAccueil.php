1e033c79a90bd3110f66bbcd6cc41f66fdb309e8
LouisMHT <128934086+LouisMHT@users.noreply.github.com>
2023-10-18 15:35:31 +0200

    Amélioration page accueil

--- GAMEULTIMATE/pageAccueil.php
+++ GAMEULTIMATE/pageAccueil.php
@@ -1,23 +1,51 @@
 <?php
-$titre = "Titre de la page";
+$titre = "Game Ultimate";
 require('header.inc.php')
 ?>
 
-
 <body>
-
-<div class="header">
-  <img src="Game-Ultimate-09-10-2023.png" alt="Logo" width=15%;>
-  
-</div>
-
 <ul>
+<li><a  href="#logo"><img src="Game-Ultimate-09-10-2023.png" alt="Logo" width=28></a></li>
   <li><a  href="#home">Accueil</a></li>
   <li><a href="#news">Jeux</a></li>
   <li><a href="#planning">Planning</a></li>
   <li><a href="#contact">Contact</a></li>
-  <li style="float:right"><a  href="#about">Connexion/Inscription</a></li>
+  <li style="float:right"><a  href="#connexion">Connexion/Inscription</a></li>
 </ul>
-<!--<img src="jeuplateau.jpeg" alt="Jeu plateau">-->
+<img src="jeuplateau.jpeg" class="full-size-image" alt="plateau">
+<img src="Game-Ultimate-09-10-2023.png" class="centered-image" alt="plateau">
+<div class="positioned-text">Bienvenue sur Game Ultimate</div>
+<div class="positioned-text2">Bienvenue sur Game Ultimate, le site de jeux le plus populaire du moment !</div>
+
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+Texte<br>
+
+
+
 </body>
 </html>
