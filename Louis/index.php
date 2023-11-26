12296b0949172acd7dc7e70988ead2462dd6b8a4
LouisMHT <128934086+LouisMHT@users.noreply.github.com>
2023-11-26 21:42:05 +0100

    Page des jeux terminée

--- Louis/index.php
+++ Louis/index.php
@@ -18,16 +18,34 @@ require('header.inc.php')
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="Page des jeux.php">Jeux</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Planning</a>
-        </li>
+
+
+        <?php
+          if (isset($_SESSION['Username'])) {
+            echo "<li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"Page des jeux.php\">Jeux</a>
+                  </li>";
+          }else{
+            echo "<li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"#\" onclick=\"executerJavascript()\">Jeux</a>
+                  </li>";
+          }
+        ?>
+
+        <?php
+          if (isset($_SESSION['Username'])) {
+            echo "<li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"#\">Planning</a>
+                  </li>";
+          }else{
+            echo "<li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"#\" onclick=\"executerJavascript()\">Planning</a>
+                  </li>";
+          }
+        ?>
         
  
         <?php
-        
           if (isset($_SESSION['Username'])) {
               echo "<li class=\"nav-item\">
                       <a class=\"nav-link\" href=\"Page de connexion.php\"> " . $_SESSION['Username'] . "</a>
@@ -67,53 +85,62 @@ require('header.inc.php')
   </div>
 </div>
 
+
 <!-- Cartes des jeux du moment -->
-<div class="container">
-  <br>  
-  <p class="fw-bold fs-2 text-secondary">Jeux du moment</p>
-  <div class="row">
+<?php
+// Connexion à la base de données
+$mysqli = new mysqli("localhost", "root", "root", "ultimategame");
+
+// Récupérer les jeux depuis la base de données
+$resultat = $mysqli->query("SELECT nomJeu, descriptionJeu, photoJeu FROM jeu");
+
+// Début du container
+echo '<div class="container">
+        <br>
+        <p class="fw-bold fs-2 text-secondary">Jeux</p>';
+
+// Compteur pour suivre le nombre de jeux par ligne
+$jeuxLigne = 0;
+
+// Début de la première ligne
+echo '<div class="row">';
+
+// Afficher les jeux dans les cartes HTML
+while ($jeu = $resultat->fetch_assoc()) {
+    echo '
     <div class="col">
-      <div class="card" style="width: 18rem;">
-        <img src="Images/Monopoly.jpg" class="card-img-top" alt="monopoly">
-        <div class="card-body">
-          <h5 class="card-title">Monopoly</h5>
-          <p class="card-text">Le Monopoly est un jeu de société sur parcours dont le but est, à travers l'achat et la vente de propriétés, de ruiner ses adversaires et ainsi parvenir au monopole.</p>
-          <a href="#" class="btn btn-primary">Détails</a>
+        <div class="card" style="width: 18rem;">
+            <img src="' . $jeu['photoJeu'] . '" class="card-img-top" alt="' . $jeu['nomJeu'] . '">
+            <div class="card-body">
+                <h5 class="card-title">' . $jeu['nomJeu'] . '</h5>
+                <p class="card-text">' . $jeu['descriptionJeu'] . '</p>
+                <a href="#" class="btn btn-primary">Détails</a>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card" style="width: 18rem;">
-        <img src="Images/Monopoly.jpg" class="card-img-top" alt="monopoly">
-        <div class="card-body">
-          <h5 class="card-title">Monopoly</h5>
-          <p class="card-text">Le Monopoly est un jeu de société sur parcours dont le but est, à travers l'achat et la vente de propriétés, de ruiner ses adversaires et ainsi parvenir au monopole.</p>
-          <a href="#" class="btn btn-primary">Détails</a>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card" style="width: 18rem;">
-        <img src="Images/Monopoly.jpg" class="card-img-top" alt="monopoly">
-        <div class="card-body">
-          <h5 class="card-title">Monopoly</h5>
-          <p class="card-text">Le Monopoly est un jeu de société sur parcours dont le but est, à travers l'achat et la vente de propriétés, de ruiner ses adversaires et ainsi parvenir au monopole.</p>
-          <a href="#" class="btn btn-primary">Détails</a>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card" style="width: 18rem;">
-        <img src="Images/Monopoly.jpg" class="card-img-top" alt="monopoly">
-        <div class="card-body">
-          <h5 class="card-title">Monopoly</h5>
-          <p class="card-text">Le Monopoly est un jeu de société sur parcours dont le but est, à travers l'achat et la vente de propriétés, de ruiner ses adversaires et ainsi parvenir au monopole.</p>
-          <a href="#" class="btn btn-primary">Détails</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+    </div>';
+
+    // Incrémenter le compteur
+    $jeuxLigne = $jeuxLigne + 1;
+
+    // Vérifier si 4 jeux ont été affichés, si oui, commencer une nouvelle ligne
+    if ($jeuxLigne === 4) {
+        echo '</div>';
+        $jeuxLigne = 0;
+        break;
+    }
+}
+
+// Fin de la dernière ligne
+echo '</div>
+      <br>';
+
+// Fin du container
+echo '</div>';
+
+// Fermeture de la connexion avec la BDD
+$mysqli->close();
+?>
+
 <br>
 <br>
 
@@ -121,6 +148,34 @@ require('header.inc.php')
 <div class="footer">
     <p>Création du site par Louis et Taher</p>
 </div>
+
+
+<script>
+  function executerJavascript() {
+    // Code JavaScript pour les notifications
+    if ('Notification' in window) {
+      Notification.requestPermission().then(function(permission) {
+        if (permission === 'granted') {
+          function afficherNotification(message) {
+            var notification = new Notification('Connexion Utilisateur', {
+              body: message,
+            });
+          }
+          afficherNotification('Connectez vous pour acceder à cette page !');
+        } else {
+          console.warn('La permission de notification n\'est pas accordée.');
+        }
+      });
+    } else {
+      console.warn('Les notifications ne sont pas prises en charge par ce navigateur.');
+    }
+  }
+</script>
+
+
+
+
+
 
 
 </body>
